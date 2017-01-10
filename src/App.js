be614bd81996e4a6bb711335dd9301eaebc66812
devendra <this.devendra@gail.com>
2017-01-10 21:08:04 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Nav from './Nav';
 
 class App extends Component {
   render() {
+    var backStyle={background: "#7F8C8D"}
+    
     return (
       <div>
         <Nav/>
         <div className="container">
           <div className="row">
-            <div className="col-xs-12">
+            <div className="col-xs-12" style={backStyle}>
               {this.props.children}
             </div> 
           </div>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -66,71 +66,79 @@ componentDidMount(){
            padding: "20px",
              margin: "20px"
         };
-        
-    return (
 
-    <div style={divStyle}> 
-        <div>
-            <h1>Movies</h1>
-        
-            <input ref={node => this.movie = node} type="text"/>
-            <button className="btn btn-danger btn-xs" onClick={this.go}>Search</button>
-            <div />
+    return (
+      <div>
+        <div class="jumbotron">
+          <h1>Movies Watch List!</h1>
+          <p>Search your movies and add them to the movies watched list</p>
         </div>
-        <div>
-            <table className="table table-striped">
+        <div style={divStyle}> 
+            <div>
+                <h1>Search Movies</h1>
+                <input ref={node => this.movie = node} type="text"/>
+                <button className="btn btn-danger btn-xs" onClick={this.go}>Search</button>
+                <div />
+            </div>
+            <div>
+                <table className="table table-striped "> 
+                    <thead className="thead-inverse">
+                        <tr>
+                            <th>Add</th>
+                            <th>IMDB ID </th>
+                            <th>Title</th>    
+                            <th>Type</th>                    
+                            <th>Year</th>
+                            <th>Poster</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {                                                                             
+                            this.state.results.map((r,i)=>{
+                                console.log(r.Title);
+                                return(
+                            
+                                    <tr key={i} >
+                                        <td><button onClick={this.add} className="btn btn-success btn-sm">Add</button></td>
+                                        <td className="imdbID">{r.imdbID}</td>
+                                        <td className="title">{r.Title}</td>
+                                        <td className="type">{r.Type}</td>
+                                        <td className="year">{r.Year}</td>
+                                        <td><img className="poster" height="100px" width="80px" src={r.Poster}/></td>
+                                    </tr>
+                                );
+                            })                          
+                        }
+                    </tbody>
+
+                </table>
+
+                
+                <h1> Movies you have already selected </h1>
+                <table className="table table-striped">
                 <thead>
-                    <tr>
-                        <th>Add</th>
-                        <th>Title</th>
-                        <th>Year</th>
-                        <th>Poster</th>
-                    </tr>
+                  <tr>
+                    <th>Title</th>
+                    <th>Year</th>
+                    <th>Poster</th>
+                  </tr>
                 </thead>
                 <tbody>
-                    {                                                                             
-                        this.state.results.map((r,i)=>{
-                            console.log(r.Title);
-                            return(
-                         
-                                <tr key={i}>
-                                    <td><button onClick={this.add} className="btn btn-success btn-xs">Add</button></td>
-                                    <td className="title">{r.Title}</td>
-                                    <td className="year">{r.Year}</td>
-                                    <td><img className="poster" src={r.Poster}/></td>
-                                </tr>
-                            );
-                        })                          
-                    }
+                  {
+                    this.state.movies.map((r, i) => {
+                      return (
+                        <tr key={i}>
+                          <td>{r.doc._id}</td>
+                          <td>{r.doc.year}</td>
+                          <td><img height="100px" width="80px" src={r.doc.poster}/></td>
+                        </tr>
+                      );
+                    })
+                  }
                 </tbody>
+              </table>
 
-            </table>
-
-            
-            <h1> Movies already selected </h1>
-            <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Year</th>
-                <th>Poster</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                this.state.movies.map((r, i) => {
-                  return (
-                    <tr key={i}>
-                      <td>{r.doc._id}</td>
-                      <td>{r.doc.year}</td>
-                      <td><img src={r.doc.poster}/></td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
-          </table>
-
+            </div>
         </div>
     </div>
     );
