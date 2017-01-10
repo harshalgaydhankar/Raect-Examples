7c11d90bbc4ca6e8687bf51311c4c923919fcb92
hemsundar9 <nirmaloasis@hotmail.com>
2017-01-10 10:54:14 +0530

    Boxes code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {this.props.children}
+      <div>
+        <Nav/>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              {this.props.children}
+            </div> 
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import {Router,  Route, IndexRoute, browserHistory} from 'react-router';
 import Home from "./components/Home";
 import About from "./components/About";
 import Faq from "./components/Faq";
+import Boxes from "./components/Boxes/Boxes";
+
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}> 
     <IndexRoute component ={Home}/>
+      <Route path="box" component={Boxes}/>
       <Route path="about" component={About}/>
       <Route path="faq" component={Faq}/>
     </Route>
