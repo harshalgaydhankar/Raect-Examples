e611d2cb5122a2faffcb738aef64ae0753c6cfdd
hemsundar9 <nirmaloasis@hotmail.com>
2017-01-10 09:45:17 +0530

    Routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import {Router,  Route, IndexRoute, browserHistory} from 'react-router';
+import Home from "./components/Home";
+import About from "./components/About";
+import Faq from "./components/Faq";
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}> 
+    <IndexRoute component ={Home}/>
+      <Route path="about" component={About}/>
+      <Route path="faq" component={Faq}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
